aa589321c135e26ab99ef75ef0251cb48891be26
hyeon-aa <ahhyun1120@naver.com>
2024-04-28 22:53:57 +0900

    feat: reservation

--- movie-reservation/src/App.js
+++ movie-reservation/src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Reservation from "./pages/Reservation/Reservation";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Main from "./pages/Reservation/Main";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header />
-      <div>
-        <br></br>
-        <br></br>
-      </div>
+      <Routes>
+          <Route path="/" element={<Main/>}></Route>
+          <Route path ="/reservation" element ={<Reservation></Reservation>}></Route>
+      </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
