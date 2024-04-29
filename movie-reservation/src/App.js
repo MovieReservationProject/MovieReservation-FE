ae2a2c8296fb815e77973d4fe28900cf88b7a8b4
hyeon-aa <66656181+hyeon-aa@users.noreply.github.com>
2024-04-29 21:38:41 +0900

    Revert "feat: reservation"

--- movie-reservation/src/App.js
+++ movie-reservation/src/App.js
@@ -2,19 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Reservation from "./pages/Reservation/Reservation";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Main from "./pages/Reservation/Main";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
       <Header />
-      <Routes>
-          <Route path="/" element={<Main/>}></Route>
-          <Route path ="/reservation" element ={<Reservation></Reservation>}></Route>
-      </Routes>
-      </BrowserRouter>
+      <div>
+        <br></br>
+        <br></br>
+      </div>
       <Footer />
     </div>
   );
