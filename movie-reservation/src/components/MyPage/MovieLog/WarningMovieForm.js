db4f819b1734063d4eea5b51c4684f5bb04ee076
Seongseri <tjdtpfl@gmail.com>
2024-05-31 20:26:53 +0900

    Fix: Update scripts section in package.json for Netlify deployment

--- movie-reservation/src/components/MyPage/MovieLog/WarningMovieForm.js
+++ movie-reservation/src/components/MyPage/MovieLog/WarningMovieForm.js
@@ -1,9 +1,10 @@
 import React from "react";
 import WarningSign from "../../../assets/png/warning-sign.png";
+import "./MovieLog.css";
 
 function WarningMovieForm() {
   return (
-    <div className="warningSign-wrap">
+    <div className="warningMovie-wrap">
       <img src={WarningSign} />
       <p>내가 본 영화가 없습니다.</p>
     </div>
